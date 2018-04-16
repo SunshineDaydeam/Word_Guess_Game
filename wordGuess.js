05a7045947dc111229f086f6730b3aa6cf24cc7e
Sunshine Daydream <alexbruner@live.com>
2018-04-15 21:43:12 -0500

    Added array lists of possible answers, pseudocoded java, and mostly finished html/css.

---
+++ wordGuess.js
@@ -0,0 +1,35 @@
+var carBrands = ["Acura", "Audi", "Bentley", "BMW", "Bugatti", "Buick", "Cadillac", "Chevrolet", "Chevy", "Chrysler", "Citroen", "Dodge", "Ferrari", "Fiat", "Ford", "Geely", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Koenigsegg", "Lamborghini", "Lexus", "Maserati", "Mazda", "Mclaren", "Mercedes", "Mitsubishi", "Pagani", "Peugot", "Porsche", "Renault", "Subaru", "Suzuki", "Tesla", "Toyota", "Volkswagen", "Volvo"]
+
+var guitarBrands = ["Alembic", "Alvarez", "Aria", "Breedlove", "Collings", "Cort", "Danelectro", "Dean", "Dobro", "Duesenberg", "Epiphone", "Fender", "Garrison", "Gibson", "Godin", "Gretsch", "Guild", "Harmony", "Heritage", "Hofner", "Hohner", "Ibanez", "Jackson", "Kramer", "Larivee", "Lowden", "Luna", "Martin", "Ovation", "Parker", "Peavey", "Rainsong", "Ramirez", "Rickenbacker", "Samick", "Seagull", "Sigma", "Squier", "Takamine", "Taylor", "Teisco", "Tacoma", "Vox", "Walden", "Warwick", "Yamaha", "Yairi"]
+
+var vegasStripCasinos = ["Aria", "Ballys", "Bellagio", "Caesars", "Cosmopolitan", "Encore", "Excalibur", "Flamingos", "Harras", "Linq", "Luxor", "Mgm", "Mirage", "Palazzo", "Paris", "Stratosphere", "Tropicana", "Venetian", "Winn"]
+
+var austinBbq = ["Franklins", "Rudys", "Valentinas", "Kerlin", "Browns", "Freedmans"]
+
+var chordTypes = ["Augmented", "Dimished", "Suspended", "Seventh", "Major", "Minor", "Power", "Ninth", "Eleventh"]
+
+var musicGenres = ["Alternative", "Americana", "Blues", "Bluegrass", "Rock", "Rockabilly", "Punk", "Grunge", "Contemporary", "Classical", "Country", "Baroque", "Choral", "Opera", "Orchestral", "Rennaissance", "Gospel", "Dance", "Dubstep", "Hardcore", "Techno", "Trance", "Reggae", "Swing", "Rap", "Christmas", "Metal", "Jazz", "Ragtime", "Karaoke", "Mariachi", "Latin", "Salsa", "Britpop", "Funk", "Motown", "Psychedelic", "Soul", "Ska", "Surf", "Indie", "Chicano", "Acapella", "Calypso"]
+
+var presidentsUS = ["Taft", "Obama", "Hoover", "Washington", "Roosevelt", "Clinton", "Bush", "Nixon", "Eisenhower", "Truman", "Coolidge", "Harding", "Wilson", "Mckinley"]
+
+var statesUS = ["Hawaii", "Washington", "Oregon", "California", "Idaho", "Nevada", "Utah", "Arizona", "Montana", "Wyoming", "Colorado", "Texas"]
+
+var name = prompt("What's your name?");
+console.log("Hello " + name);
+
+//Pick a random word
+//While the word has not been guessed {
+ //Show the player their current progress
+ //Get a guess from the player
+ //If the player wants to quit the game {
+ //Quit the game
+ //}
+ //Else If the guess is not a single letter {
+ //Tell the player to pick a single letter
+ //}
+ //Else {
+ //If the guess is in the word {
+ //Update the player's progress with the guess
+ //}
+ //}
+//}
