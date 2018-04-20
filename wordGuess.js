64d0996e3ffa8ca0a5ed7388423f37bcfbda8388
Sunshine Daydream <alexbruner@live.com>
2018-04-20 04:15:40 -0500

    90% of the way theregit add -A! its only 4:15am

--- wordGuess.js
+++ wordGuess.js
@@ -1,35 +1,134 @@
-var carBrands = ["Acura", "Audi", "Bentley", "BMW", "Bugatti", "Buick", "Cadillac", "Chevrolet", "Chevy", "Chrysler", "Citroen", "Dodge", "Ferrari", "Fiat", "Ford", "Geely", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Koenigsegg", "Lamborghini", "Lexus", "Maserati", "Mazda", "Mclaren", "Mercedes", "Mitsubishi", "Pagani", "Peugot", "Porsche", "Renault", "Subaru", "Suzuki", "Tesla", "Toyota", "Volkswagen", "Volvo"]
 
-var guitarBrands = ["Alembic", "Alvarez", "Aria", "Breedlove", "Collings", "Cort", "Danelectro", "Dean", "Dobro", "Duesenberg", "Epiphone", "Fender", "Garrison", "Gibson", "Godin", "Gretsch", "Guild", "Harmony", "Heritage", "Hofner", "Hohner", "Ibanez", "Jackson", "Kramer", "Larivee", "Lowden", "Luna", "Martin", "Ovation", "Parker", "Peavey", "Rainsong", "Ramirez", "Rickenbacker", "Samick", "Seagull", "Sigma", "Squier", "Takamine", "Taylor", "Teisco", "Tacoma", "Vox", "Walden", "Warwick", "Yamaha", "Yairi"]
 
-var vegasStripCasinos = ["Aria", "Ballys", "Bellagio", "Caesars", "Cosmopolitan", "Encore", "Excalibur", "Flamingos", "Harras", "Linq", "Luxor", "Mgm", "Mirage", "Palazzo", "Paris", "Stratosphere", "Tropicana", "Venetian", "Winn"]
+    var winCount = 0;
+    var lossCount = 0;
 
-var austinBbq = ["Franklins", "Rudys", "Valentinas", "Kerlin", "Browns", "Freedmans"]
+        
+    //letters available
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var chordTypes = ["Augmented", "Dimished", "Suspended", "Seventh", "Major", "Minor", "Power", "Ninth", "Eleventh"]
+    //possible categories and words  
+    carBrands = ["acura", "audi", "bentley", "bmw", "bugatti", "buick", "cadillac", "chevrolet", "chevy", 
+    "chrysler", "citroen", "dodge", "ferrari", "fiat", "ford", "geely", "honda", "hyundai", "infiniti", "jaguar", 
+    "jeep", "koenigsegg", "lamborghini", "lexus", "maserati", "mazda", "mclaren", "mercedes", "mitsubishi", "pagani", 
+    "peugot", "porsche", "renault", "subaru", "suzuki", "tesla", "toyota", "volkswagen", "volvo"]
+    guitarBrands = ["alembic", "alvarez", "aria", "breedlove", "collings", "cort", "danelectro", "dean", "dobro", 
+    "duesenberg", "epiphone", "fender", "garrison", "gibson", "godin", "gretsch", "guild", "harmony", "heritage", 
+    "hofner", "hohner", "ibanez", "jackson", "kramer", "larivee", "lowden", "luna", "martin", "ovation", "parker", 
+    "peavey", "rainsong", "ramirez", "rickenbacker", "samick", "seagull", "sigma", "squier", "takamine", "taylor", 
+    "teisco", "tacoma", "vox", "walden", "warwick", "yamaha", "yairi"]
+    vegasStripCasinos = ["aria", "ballys", "bellagio", "caesars", "cosmopolitan", "encore", "excalibur", "flamingos", 
+    "harras", "linq", "luxor", "mgm", "mirage", "palazzo", "paris", "stratosphere", "tropicana", "venetian", "winn"],
+    austinBbq = ["franklins", "rudys", "valentinas", "kerlin", "browns", "freedmans"]
+    chordTypes = ["augmented", "diminished", "suspended", "seventh", "major", "minor", "power", "ninth", "eleventh"]
+    musicGenres = ["alternative", "americana", "blues", "bluegrass", "rock", "rockabilly", "punk", "grunge", 
+    "contemporary", "classical", "country", "baroque", "choral", "opera", "orchestral", "rennaissance", "gospel", 
+    "dance", "dubstep", "hardcore", "techno", "trance", "reggae", "swing", "rap", "christmas", "metal", "jazz", 
+    "ragtime", "karaoke", "mariachi", "latin", "salsa", "britpop", "funk", "motown", "psychedelic", "soul", "ska", 
+    "surf", "indie", "chicano", "acapella", "calypso"],
+    presidentsUs = ["taft", "obama", "hoover", "washington", "roosevelt", "clinton", "bush", "nixon", "eisenhower", 
+    "truman", "coolidge", "harding", "wilson", "mckinley"]
+    statesUs = ["hawaii", "washington", "oregon", "california", "idaho", "nevada", "utah", "arizona", "montana", 
+    "wyoming", "colorado", "texas"]
 
-var musicGenres = ["Alternative", "Americana", "Blues", "Bluegrass", "Rock", "Rockabilly", "Punk", "Grunge", "Contemporary", "Classical", "Country", "Baroque", "Choral", "Opera", "Orchestral", "Rennaissance", "Gospel", "Dance", "Dubstep", "Hardcore", "Techno", "Trance", "Reggae", "Swing", "Rap", "Christmas", "Metal", "Jazz", "Ragtime", "Karaoke", "Mariachi", "Latin", "Salsa", "Britpop", "Funk", "Motown", "Psychedelic", "Soul", "Ska", "Surf", "Indie", "Chicano", "Acapella", "Calypso"]
+    var lives = 10;         // Number of tries to guess the word
+    var userGuess="";       // User's current Guess
+    var prevGuesses=[];     // User's total previous Guesses
+    var wrongGuesses =[];   // User's incorrect guesses
 
-var presidentsUS = ["Taft", "Obama", "Hoover", "Washington", "Roosevelt", "Clinton", "Bush", "Nixon", "Eisenhower", "Truman", "Coolidge", "Harding", "Wilson", "Mckinley"]
 
-var statesUS = ["Hawaii", "Washington", "Oregon", "California", "Idaho", "Nevada", "Utah", "Arizona", "Montana", "Wyoming", "Colorado", "Texas"]
+    //computer chooses a random category
+    var category = [carBrands, guitarBrands, vegasStripCasinos, austinBbq, chordTypes, musicGenres, presidentsUs, 
+    statesUs];
+    var randomCategory = category[Math.floor(Math.random() * Math.floor(category.length))];
+    var word = randomCategory[Math.floor(Math.random() * Math.floor(randomCategory.length))];
+    console.log("new word: " + '"' + word + '"');
 
-var name = prompt("What's your name?");
-console.log("Hello " + name);
+    //create an initial answerArray
+    var answerArray = [];
+        for (i=0; i<word.length; i++){
+            answerArray[i] = "_";
+        }
 
-//Pick a random word
-//While the word has not been guessed {
- //Show the player their current progress
- //Get a guess from the player
- //If the player wants to quit the game {
- //Quit the game
- //}
- //Else If the guess is not a single letter {
- //Tell the player to pick a single letter
- //}
- //Else {
- //If the guess is in the word {
- //Update the player's progress with the guess
- //}
- //}
-//}
+        //
+        //GAME BEGINS
+        //
+
+//When key is pressed, begin the Game   
+document.onkeyup = function(event) {                                    
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();   //convert type to lowercase letter
+    if (alphabet.indexOf(userGuess) >= 0){                              //check if the user Guess is a valid letter
+        //console.log ('"Users Typed Guess:"' + userGuess);
+
+        //check if it has already been guessed
+        if (prevGuesses.indexOf(userGuess) < 0){
+            prevGuesses.push(userGuess);                                //push userGuess to prevGuesses
+            //console.log (prevGuesses);                                
+            
+
+            //  CORRECT GUESS //
+            if (word.indexOf(userGuess) >= 0){                          
+                console.log(userGuess + " is correct");                 
+                answerArray.splice(word.indexOf(userGuess), 1, userGuess);
+                for (i=0; i<word.length; i++){
+                    if  (word[i] == userGuess){
+                        answerArray.splice(i, 1, userGuess);
+                    }
+                }
+                console.log (answerArray);
+
+                // WIN //
+                if (answerArray.indexOf("_") < 0){
+                    winCount++;
+                    //Reset Word/Category/Lives/Guess Arrays
+                    randomCategory = category[Math.floor(Math.random() * Math.floor(category.length))];
+                    word = randomCategory[Math.floor(Math.random() * Math.floor(randomCategory.length))];
+                    console.log("word: " + '"' + word + "'" + " Wins: " + winCount + " Losses: " + lossCount);
+                    lives=10;
+                    prevGuesses=[];
+                    wrongGuesses=[];
+                    for (i=0; i<word.length; i++){
+                        answerArray[i] = "_";
+                    }
+                }
+
+            }
+            
+
+            //  INCORRECT GUESS //
+            else {
+                
+                //check if it has already been guessed                 
+                if (wrongGuesses.indexOf(userGuess) < 0){
+                    wrongGuesses.push(userGuess);                       //push userGuess to prevGuesses
+                    lives--;                                            //Lose One Life
+                    wrongGuesses.sort();                                //alphabetize the wrong guesses                               
+                    console.log("wrongGuesses: " + wrongGuesses + " Lives Left: " + lives)
+            
+                    //  LOSS!  //
+                    if (lives == 0){
+                        lossCount++;
+                        //Reset Word/Category/Lives/Guess Arrays
+                        randomCategory = category[Math.floor(Math.random() * Math.floor(category.length))];
+                        word = randomCategory[Math.floor(Math.random() * Math.floor(randomCategory.length))];
+                        console.log("word: " + '"' + word + "'" + " LossCount: " + lossCount);
+                        lives=10;
+                        prevGuesses=[];
+                        wrongGuesses=[];
+                        for (i=0; i<word.length; i++){
+                            answerArray[i] = "_";
+                        }
+                    }
+                }
+
+                    
+
+            }
+        }
+    }
+}
+
+
+    
